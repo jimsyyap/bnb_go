8f7e4db65647b479c3ee5ce36597ba34f8717bd5
jimmy desu <jimsyyap@gmail.com>
2024-07-31 09:11:19 +1000

    templates joy

--- main.go
+++ main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
-func main() {
-	saySomething("Hello, World!")
-	// fmt.Println(saySomething("Hello, World!"))
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
 }
 
-func saySomething(s string) string {
-	fmt.Println(s)
-	return s
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	fmt.Println("Starting application on port", portNumber)
+
+	_ = http.ListenAndServe(portNumber, nil)
 }
