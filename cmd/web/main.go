cae0e935034e8e058a783380f72efa850da67228
jimmy desu <jimsyyap@gmail.com>
2024-07-31 15:53:08 +1000

    updated main, render == joy

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/jimsyyap/bnb_go/pkg/config"
 	"github.com/jimsyyap/bnb_go/pkg/handlers"
+	"github.com/jimsyyap/bnb_go/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 	fmt.Println("Starting application on port", portNumber)
